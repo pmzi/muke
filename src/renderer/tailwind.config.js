a65775c9121dc3230fbbe85f2604558706a61131
Pouya MozaffarMagham <pmzi@Pouyas-MacBook-Pro.local>
2020-10-16 17:31:07 +0330

    Add tailwindcss

---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [
+    './src/**/*.scss',
+    './src/**/*.css',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
