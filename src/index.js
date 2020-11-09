cecce51f121179e126e1385b7ee8c128949b98ba
Pouya <pouya.mozafar@gmail.com>
2020-11-09 23:29:29 +0330

    Remove electron-reloader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const mainApp = require('./main/app');
 
-// try {
-//   require('electron-reloader')(module);
-// } catch (_) {}
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
