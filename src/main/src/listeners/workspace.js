f1807ca65a98b7aeab44ab2f23904485ec674a44
Pouya <pouya.mozafar@gmail.com>
2020-11-09 22:14:40 +0330

    Make use of success method inside workspace listener

--- src/main/src/listeners/workspace.js
+++ src/main/src/listeners/workspace.js
@@ -3,13 +3,13 @@ const { CREATE_WORKSPACE } = require('@shared/constants/listenerPaths');
 const workspaceController = require('@controllers/workspace');
 
 module.exports = function registerListeners({ listen }) {
-  listen(CREATE_WORKSPACE, async (event, {
+  listen(CREATE_WORKSPACE, async (res, {
     name, address, port, proxy,
   }) => {
     const newWorkspace = await workspaceController.createWorkspace({
       name, address, port, proxy,
     });
 
-    event.reply(CREATE_WORKSPACE, newWorkspace.toJSON());
+    res.success(CREATE_WORKSPACE, newWorkspace.toJSON());
   });
 };
