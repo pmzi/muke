c1d4e8bd28aff5834469409cb40fca704f20a012
Pouya <pouya.mozafar@gmail.com>
2020-11-09 22:14:03 +0330

    Declare success and error methods in connector

--- src/main/src/services/connector.js
+++ src/main/src/services/connector.js
@@ -1,5 +1,22 @@
 const { ipcMain } = require('electron');
 
 exports.listen = function listen(eventName, cb) {
-  ipcMain.on(eventName, cb);
+  ipcMain.on(eventName, (event, ...args) => {
+    const res = {
+      success(channel, data) {
+        event.reply(channel, {
+          status: 'success',
+          body: data,
+        });
+      },
+      error(channel, data) {
+        event.reply(channel, {
+          status: 'error',
+          body: data,
+        });
+      },
+    };
+
+    cb(res, ...args);
+  });
 };
