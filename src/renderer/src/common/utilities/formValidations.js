118a132540bd65bc21f228b94d7e8af72d764b15
Pouya <pouya.mozafar@gmail.com>
2020-11-22 13:45:03 +0330

    Add url and number validations

--- src/renderer/src/common/utilities/formValidations.js
+++ src/renderer/src/common/utilities/formValidations.js
@@ -1,7 +1,21 @@
-// eslint-disable-next-line import/prefer-default-export
 export function required(message = 'This field is required!') {
   return {
     required: true,
+    whitespace: true,
+    message,
+  };
+}
+
+export function url(message = 'Enter a valid URL!') {
+  return {
+    type: 'url',
+    message,
+  };
+}
+
+export function number(message = 'Enter a valid number!') {
+  return {
+    pattern: /^\d+$/,
     message,
   };
 }
