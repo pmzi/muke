d5181574ec581389bdd864b89a8ba0260b00a258
Pouya <pouya.mozafar@gmail.com>
2020-11-26 23:02:46 +0330

    Remove log

--- src/renderer/src/services/dispatch/index.js
+++ src/renderer/src/services/dispatch/index.js
@@ -3,7 +3,8 @@ import ApiError from './ApiError';
 
 export default function dispatch(eventName, data) {
   return dispatcher(eventName, data).then((res) => {
-    if (res.status === 'success') return res;
+    console.log(res);
+    if (res.status === 'success') return res.body;
     throw new ApiError({ message: 'An error occurred.', details: res.body });
   });
 }
