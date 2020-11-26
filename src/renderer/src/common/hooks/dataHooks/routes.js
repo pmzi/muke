4925bb1e9b90daf70f2b25cb88eac98ea7e34f07
Pouya <pouya.mozafar@gmail.com>
2020-11-26 23:01:56 +0330

    Add data hooks of the add route/group

--- src/renderer/src/common/hooks/dataHooks/routes.js
+++ src/renderer/src/common/hooks/dataHooks/routes.js
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@common/hooks/dataHandlerHooks';
+import { useMutation, useQuery, queryCache } from '@common/hooks/dataHandlerHooks';
 import { routes } from '@/api';
 import { GET_ROUTES } from '@common/constants/queries';
 
@@ -8,4 +8,58 @@ export function useGetRoutes(id, options) {
 
 export function useChangeRoutesOrders(options) {
   return useMutation(routes.changeRoutesOrders, options);
+}
+
+export function useAddRoute(options) {
+  return useMutation(routes.addRoute, {
+    onSuccess: ({ id, name: text, parent }) => {
+      queryCache.setQueryData(
+        GET_ROUTES,
+        (oldData) => {
+          if (parent) {
+            return oldData.map((route) => {
+              if (route.children && route.id === parent) {
+                route.children.push({
+                  id, text,
+                });
+              }
+              return route;
+            });
+          }
+          return [
+            ...oldData,
+            {
+              id, text,
+            },
+          ];
+        },
+      );
+    },
+    ...options,
+  });
+}
+
+export function useAddRouteGroup(options) {
+  return useMutation(routes.addRouteGroup, {
+    onSuccess: ({ id, label: title }) => {
+      queryCache.setQueryData(
+        GET_ROUTES,
+        (oldData) => {
+          console.log([
+            ...oldData,
+            {
+              id, title, children: [],
+            },
+          ]);
+          return [
+            ...oldData,
+            {
+              id, title, children: [],
+            },
+          ];
+        },
+      );
+    },
+    ...options,
+  });
 }
