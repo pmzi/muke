c965476f5397f497cca494f890da6b93e896100a
Pouya <pouya.mozafar@gmail.com>
2020-12-03 19:22:23 +0330

    Change pipeline structure

--- src/renderer/src/services/pipeline.js
+++ src/renderer/src/services/pipeline.js
@@ -3,13 +3,9 @@ import { ipcRenderer } from 'electron';
 
 function on(channel, cb) {
   ipcRenderer.on(channel, (e, ...args) => cb(...args));
-}
-
-function removeListener(channel, cb) {
-  ipcRenderer.removeListener(channel, cb);
+  return () => ipcRenderer.removeListener(channel, cb);
 }
 
 export default {
   on,
-  removeListener,
 };
